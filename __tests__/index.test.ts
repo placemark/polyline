9c5945327d37a4f097a8d2fde72bbacd5eff3b90
Tom MacWright <tom@macwright.com>
2025-11-16 17:23:14 -0500

    Switch to biome

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,169 +1,169 @@
-import { describe, expect, it } from "vitest";
 import type { LineString } from "geojson";
+import { describe, expect, it } from "vitest";
 import * as polyline from "../src/index.js";
 
 const example = [
-  [-120.2, 38.5],
-  [-120.95, 40.7],
-  [-126.453, 43.252],
+	[-120.2, 38.5],
+	[-120.95, 40.7],
+	[-126.453, 43.252],
 ];
 
 const example_with_precision = [
-  [-12.02, 3.85],
-  [-12.095, 4.07],
-  [-12.6453, 4.3252],
+	[-12.02, 3.85],
+	[-12.095, 4.07],
+	[-12.6453, 4.3252],
 ];
 
 const exampleWithZ = [
-  [-120.2, 38.5, 0],
-  [-120.95, 40.7, 0],
-  [-126.453, 43.252, 0],
+	[-120.2, 38.5, 0],
+	[-120.95, 40.7, 0],
+	[-126.453, 43.252, 0],
 ];
 
 // encoded value will enclude slashes -> tests escaping
 const example_slashes = [
-  [-82.55, 35.6],
-  [-82.55015, 35.59985],
-  [-82.55, 35.6],
+	[-82.55, 35.6],
+	[-82.55015, 35.59985],
+	[-82.55, 35.6],
 ];
 
 const example_rounding = [
-  [0.000006, 0],
-  [0.000002, 0],
+	[0.000006, 0],
+	[0.000002, 0],
 ];
 
 const example_rounding_negative = [
-  [-112.084004, 36.05322],
-  [-112.083914, 36.053573],
-  [-112.083965, 36.053845],
+	[-112.084004, 36.05322],
+	[-112.083914, 36.053573],
+	[-112.083965, 36.053845],
 ];
 
 const geojson: LineString = {
-  type: "LineString",
-  coordinates: example,
+	type: "LineString",
+	coordinates: example,
 };
 
 const geojson_with_precision: LineString = {
-  type: "LineString",
-  coordinates: example_with_precision,
+	type: "LineString",
+	coordinates: example_with_precision,
 };
 
 describe("#decode()", () => {
-  it("decodes an empty Array", () => {
-    expect(polyline.decode("")).toEqual([]);
-  });
+	it("decodes an empty Array", () => {
+		expect(polyline.decode("")).toEqual([]);
+	});
 
-  it("decodes any string, but coordinates will be weird", () => {
-    expect(polyline.decode("Hello, world!")).toEqual([
-      [70.84755, -0.00005],
-      [434.83615, -0.00006],
-    ]);
-  });
+	it("decodes any string, but coordinates will be weird", () => {
+		expect(polyline.decode("Hello, world!")).toEqual([
+			[70.84755, -0.00005],
+			[434.83615, -0.00006],
+		]);
+	});
 
-  it("decodes any string, but coordinates will be weird with custom precision", () => {
-    expect(polyline.decode("Hello, world!", 6)).toEqual([
-      [7.084755, -0.000005],
-      [43.483615, -0.000006],
-    ]);
-  });
+	it("decodes any string, but coordinates will be weird with custom precision", () => {
+		expect(polyline.decode("Hello, world!", 6)).toEqual([
+			[7.084755, -0.000005],
+			[43.483615, -0.000006],
+		]);
+	});
 
-  it("decodes a String into an Array of lat/lon pairs", () => {
-    expect(polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@")).toEqual(example);
-  });
+	it("decodes a String into an Array of lat/lon pairs", () => {
+		expect(polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@")).toEqual(example);
+	});
 
-  it("decodes a String into an Array of lat/lon pairs with custom precision", () => {
-    expect(polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@", 6)).toEqual(
-      example_with_precision,
-    );
-  });
+	it("decodes a String into an Array of lat/lon pairs with custom precision", () => {
+		expect(polyline.decode("_p~iF~ps|U_ulLnnqC_mqNvxq`@", 6)).toEqual(
+			example_with_precision,
+		);
+	});
 });
 
 describe("#identity", () => {
-  it("feed encode into decode and check if the result is the same as the input", () => {
-    expect(polyline.decode(polyline.encode(example_slashes))).toEqual(
-      example_slashes,
-    );
-  });
+	it("feed encode into decode and check if the result is the same as the input", () => {
+		expect(polyline.decode(polyline.encode(example_slashes))).toEqual(
+			example_slashes,
+		);
+	});
 
-  it("feed decode into encode and check if the result is the same as the input", () => {
-    expect(polyline.encode(polyline.decode("_chxEn`zvN\\\\]]"))).toEqual(
-      "_chxEn`zvN\\\\]]",
-    );
-  });
+	it("feed decode into encode and check if the result is the same as the input", () => {
+		expect(polyline.encode(polyline.decode("_chxEn`zvN\\\\]]"))).toEqual(
+			"_chxEn`zvN\\\\]]",
+		);
+	});
 });
 
 describe("#encode()", () => {
-  it("encodes an empty Array", () => {
-    expect(polyline.encode([])).toEqual("");
-  });
+	it("encodes an empty Array", () => {
+		expect(polyline.encode([])).toEqual("");
+	});
 
-  it("encodes an Array of lat/lon pairs into a String", () => {
-    expect(polyline.encode(example)).toEqual("_p~iF~ps|U_ulLnnqC_mqNvxq`@");
-  });
+	it("encodes an Array of lat/lon pairs into a String", () => {
+		expect(polyline.encode(example)).toEqual("_p~iF~ps|U_ulLnnqC_mqNvxq`@");
+	});
 
-  it("encodes an Array of lat/lon pairs into a String with custom precision", () => {
-    expect(polyline.encode(example, 6)).toEqual(
-      "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
-    );
-  });
+	it("encodes an Array of lat/lon pairs into a String with custom precision", () => {
+		expect(polyline.encode(example, 6)).toEqual(
+			"_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
+		);
+	});
 
-  it("encodes an Array of lat/lon/z into the same string as lat/lon", () => {
-    expect(polyline.encode(exampleWithZ)).toEqual(
-      "_p~iF~ps|U_ulLnnqC_mqNvxq`@",
-    );
-  });
+	it("encodes an Array of lat/lon/z into the same string as lat/lon", () => {
+		expect(polyline.encode(exampleWithZ)).toEqual(
+			"_p~iF~ps|U_ulLnnqC_mqNvxq`@",
+		);
+	});
 
-  it("encodes an Array of lat/lon/z into the same string as lat/lon with custom precision", () => {
-    expect(polyline.encode(exampleWithZ, 6)).toEqual(
-      "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
-    );
-  });
+	it("encodes an Array of lat/lon/z into the same string as lat/lon with custom precision", () => {
+		expect(polyline.encode(exampleWithZ, 6)).toEqual(
+			"_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
+		);
+	});
 
-  it("encodes with proper rounding", () => {
-    expect(polyline.encode(example_rounding)).toEqual("?A?@");
-  });
+	it("encodes with proper rounding", () => {
+		expect(polyline.encode(example_rounding)).toEqual("?A?@");
+	});
 
-  it("encodes with proper rounding with custom precision", () => {
-    expect(polyline.encode(example_rounding, 6)).toEqual("?K?F");
-  });
+	it("encodes with proper rounding with custom precision", () => {
+		expect(polyline.encode(example_rounding, 6)).toEqual("?K?F");
+	});
 
-  it("encodes with proper negative rounding", () => {
-    expect(polyline.encode(example_rounding_negative)).toEqual(
-      "ss`{E~kbkTeAQw@J",
-    );
-  });
+	it("encodes with proper negative rounding", () => {
+		expect(polyline.encode(example_rounding_negative)).toEqual(
+			"ss`{E~kbkTeAQw@J",
+		);
+	});
 
-  it("encodes with proper negative rounding with custom precision", () => {
-    expect(polyline.encode(example_rounding_negative, 6)).toEqual(
-      "gmowcAfaaxtEaUsD_PdB",
-    );
-  });
+	it("encodes with proper negative rounding with custom precision", () => {
+		expect(polyline.encode(example_rounding_negative, 6)).toEqual(
+			"gmowcAfaaxtEaUsD_PdB",
+		);
+	});
 });
 
 describe("#fromGeoJSON()", () => {
-  it("allows geojson geometries", () => {
-    expect(polyline.geoJSONToPolyline(geojson)).toEqual(
-      "_p~iF~ps|U_ulLnnqC_mqNvxq`@",
-    );
-  });
+	it("allows geojson geometries", () => {
+		expect(polyline.geoJSONToPolyline(geojson)).toEqual(
+			"_p~iF~ps|U_ulLnnqC_mqNvxq`@",
+		);
+	});
 
-  it("allows geojson geometries with custom precision", () => {
-    expect(polyline.geoJSONToPolyline(geojson, 6)).toEqual(
-      "_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
-    );
-  });
+	it("allows geojson geometries with custom precision", () => {
+		expect(polyline.geoJSONToPolyline(geojson, 6)).toEqual(
+			"_izlhA~rlgdF_{geC~ywl@_kwzCn`{nI",
+		);
+	});
 });
 
 describe("#toGeoJSON()", () => {
-  it("flips coordinates and decodes geometry", () => {
-    expect(polyline.polylineToGeoJSON("_p~iF~ps|U_ulLnnqC_mqNvxq`@")).toEqual(
-      geojson,
-    );
-  });
+	it("flips coordinates and decodes geometry", () => {
+		expect(polyline.polylineToGeoJSON("_p~iF~ps|U_ulLnnqC_mqNvxq`@")).toEqual(
+			geojson,
+		);
+	});
 
-  it("flips coordinates and decodes geometry with custom precision", () => {
-    expect(
-      polyline.polylineToGeoJSON("_p~iF~ps|U_ulLnnqC_mqNvxq`@", 6),
-    ).toEqual(geojson_with_precision);
-  });
+	it("flips coordinates and decodes geometry with custom precision", () => {
+		expect(
+			polyline.polylineToGeoJSON("_p~iF~ps|U_ulLnnqC_mqNvxq`@", 6),
+		).toEqual(geojson_with_precision);
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -8,30 +8,30 @@ import type { LineString, Position } from "geojson";
 // by [Mark McClure](http://facstaff.unca.edu/mcmcclur/)
 
 function py2_round(value: number) {
-  // Google's polyline algorithm uses the same rounding strategy as Python 2,
-  // which is different from JS for negative values
-  return Math.floor(Math.abs(value) + 0.5) * (value >= 0 ? 1 : -1);
+	// Google's polyline algorithm uses the same rounding strategy as Python 2,
+	// which is different from JS for negative values
+	return Math.floor(Math.abs(value) + 0.5) * (value >= 0 ? 1 : -1);
 }
 
 function encodeNumber(current: number, previous: number, factor: number) {
-  current = py2_round(current * factor);
-  previous = py2_round(previous * factor);
-  let coordinate = current - previous;
-  coordinate <<= 1;
-  if (current - previous < 0) {
-    coordinate = ~coordinate;
-  }
-  let output = "";
-  while (coordinate >= 0x20) {
-    output += String.fromCharCode((0x20 | (coordinate & 0x1f)) + 63);
-    coordinate >>= 5;
-  }
-  output += String.fromCharCode(coordinate + 63);
-  return output;
+	current = py2_round(current * factor);
+	previous = py2_round(previous * factor);
+	let coordinate = current - previous;
+	coordinate <<= 1;
+	if (current - previous < 0) {
+		coordinate = ~coordinate;
+	}
+	let output = "";
+	while (coordinate >= 0x20) {
+		output += String.fromCharCode((0x20 | (coordinate & 0x1f)) + 63);
+		coordinate >>= 5;
+	}
+	output += String.fromCharCode(coordinate + 63);
+	return output;
 }
 
 function resultChange(result: number) {
-  return result & 1 ? ~(result >> 1) : result >> 1;
+	return result & 1 ? ~(result >> 1) : result >> 1;
 }
 
 /**
@@ -42,52 +42,52 @@ function resultChange(result: number) {
  * outside of the normal range.
  */
 export function decode(str: string, precision: number = 5): Position[] {
-  const factor = Math.pow(10, precision);
-  let index = 0;
-  let lat = 0;
-  let lng = 0;
-  const coordinates = [];
-  let shift = 0;
-  let result = 0;
-  let byte = null;
+	const factor = 10 ** precision;
+	let index = 0;
+	let lat = 0;
+	let lng = 0;
+	const coordinates = [];
+	let shift = 0;
+	let result = 0;
+	let byte = null;
 
-  let latitude_change: number;
-  let longitude_change: number;
+	let latitude_change: number;
+	let longitude_change: number;
 
-  // Coordinates have variable length when encoded, so just keep
-  // track of whether we've hit the end of the string. In each
-  // loop iteration, a single coordinate is decoded.
-  while (index < str.length) {
-    // Reset shift, result, and byte
-    byte = null;
-    shift = 0;
-    result = 0;
+	// Coordinates have variable length when encoded, so just keep
+	// track of whether we've hit the end of the string. In each
+	// loop iteration, a single coordinate is decoded.
+	while (index < str.length) {
+		// Reset shift, result, and byte
+		byte = null;
+		shift = 0;
+		result = 0;
 
-    do {
-      byte = str.charCodeAt(index++) - 63;
-      result |= (byte & 0x1f) << shift;
-      shift += 5;
-    } while (byte >= 0x20);
+		do {
+			byte = str.charCodeAt(index++) - 63;
+			result |= (byte & 0x1f) << shift;
+			shift += 5;
+		} while (byte >= 0x20);
 
-    latitude_change = resultChange(result);
+		latitude_change = resultChange(result);
 
-    shift = result = 0;
+		shift = result = 0;
 
-    do {
-      byte = str.charCodeAt(index++) - 63;
-      result |= (byte & 0x1f) << shift;
-      shift += 5;
-    } while (byte >= 0x20);
+		do {
+			byte = str.charCodeAt(index++) - 63;
+			result |= (byte & 0x1f) << shift;
+			shift += 5;
+		} while (byte >= 0x20);
 
-    longitude_change = resultChange(result);
+		longitude_change = resultChange(result);
 
-    lat += latitude_change;
-    lng += longitude_change;
+		lat += latitude_change;
+		lng += longitude_change;
 
-    coordinates.push([lng / factor, lat / factor]);
-  }
+		coordinates.push([lng / factor, lat / factor]);
+	}
 
-  return coordinates;
+	return coordinates;
 }
 
 /**
@@ -97,23 +97,23 @@ export function decode(str: string, precision: number = 5): Position[] {
  * @returns encoded polyline
  */
 export function encode(coordinates: number[][], precision: number = 5) {
-  if (!coordinates.length) {
-    return "";
-  }
-  const factor = Math.pow(10, precision);
+	if (!coordinates.length) {
+		return "";
+	}
+	const factor = 10 ** precision;
 
-  let output =
-    encodeNumber(coordinates[0]![1]!, 0, factor) +
-    encodeNumber(coordinates[0]![0]!, 0, factor);
+	let output =
+		encodeNumber(coordinates[0]![1]!, 0, factor) +
+		encodeNumber(coordinates[0]![0]!, 0, factor);
 
-  for (let i = 1; i < coordinates.length; i++) {
-    const a = coordinates[i]!;
-    const b = coordinates[i - 1]!;
-    output += encodeNumber(a[1]!, b[1]!, factor);
-    output += encodeNumber(a[0]!, b[0]!, factor);
-  }
+	for (let i = 1; i < coordinates.length; i++) {
+		const a = coordinates[i]!;
+		const b = coordinates[i - 1]!;
+		output += encodeNumber(a[1]!, b[1]!, factor);
+		output += encodeNumber(a[0]!, b[0]!, factor);
+	}
 
-  return output;
+	return output;
 }
 
 /**
@@ -122,7 +122,7 @@ export function encode(coordinates: number[][], precision: number = 5) {
  * @param geojson A LineString
  */
 export function geoJSONToPolyline(geojson: LineString, precision: number = 5) {
-  return encode(geojson.coordinates, precision);
+	return encode(geojson.coordinates, precision);
 }
 
 /**
@@ -131,12 +131,12 @@ export function geoJSONToPolyline(geojson: LineString, precision: number = 5) {
  * @param str An encoded polyline as a string.
  */
 export function polylineToGeoJSON(
-  str: string,
-  precision: number = 5,
+	str: string,
+	precision: number = 5,
 ): LineString {
-  const coords = decode(str, precision);
-  return {
-    type: "LineString",
-    coordinates: coords,
-  };
+	const coords = decode(str, precision);
+	return {
+		type: "LineString",
+		coordinates: coords,
+	};
 }
